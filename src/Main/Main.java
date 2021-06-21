939d6ff558774545896f83f1dbdba945b1ca166c
clinton.oyedeji <82894186+Zupster56@users.noreply.github.com>
2021-06-21 11:25:31 +0000

    g

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,7 +11,26 @@ package Main;
  */
 public class Main {
     public static void main(String[] args){
-        
+    //TODO code application logic header
+    Scanner in = new Scanner{System.in};
+    //det a word from the user
+    //print the following 
+    /*
+    * c a t *
+    t * * * c 
+    a * * * a 
+    c * * * t 
+    * t a c *
+    */
+    System.out.printin{"Enter a word"};
+    String input=in.nextLine();
+    //part 1: print the word surrounded by two stars
+    System.out.print{"* "};
+    for {int x=0;x<input.length();x++}{
+      System.out.print{input.charAt(x)+" ");
+
     }
-    
+     System.out.printin("*");
+
+
 }
